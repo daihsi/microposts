11e5d638ecbeccbdaa44aaf62d06ca22878b8ea9
daihsi <isitomo.mm@gmail.com>
2022-03-10 08:46:06 +0900

    add ユーザー登録機能、ログインアウト機能

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  before_action :already_login?, only: [:new, :create]
+  before_action :login?, only: [:show]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:uid] = @user.uid
+      redirect_to action: 'show', uid: session[:uid]
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @user = User.find_by(uid: params[:uid])
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root "home#top"
+  get '/users/new', to: 'users#new'
+  get '/users/:uid', to: 'users#show', as: 'users_show'
+  post '/users', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
