9e3b4459c350dabba2e66bac408af8048e1e44e3
daihsi <isitomo.mm@gmail.com>
2022-03-18 00:36:47 +0900

    add ログアウトボタンをhelper化

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,9 @@
 module SessionsHelper
+  def log_out_button
+    html = ''
+    unless current_user.nil?
+      html = button_to 'ログアウト', logout_path,  method: :delete, class: 'btn btn-outline-secondary'
+    end
+    raw html
+  end
 end
